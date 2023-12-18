769a5c03ae07f85e9817d5159e60ad4920a5624b
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-18 22:56:50 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,11 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+que = {}
+admins = {}
+
+API_ID = int(getenv("API_ID", "11163590"))
+API_HASH = getenv("API_HASH", "fa76f31e5f7a64d906d4978dd0e5d3b3")
+BOT_TOKEN = getenv("BOT_TOKEN","6932413042:AAGX738CQmb0BiMGxp9VP5NweIstBwKLdfA")
+BOT_NAME = getenv("BOT_NAME","Tg Youtube Download")
