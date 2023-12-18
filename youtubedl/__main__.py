bd51020b443c3b2f3ddda01440c026b8e6b115c0
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-18 23:17:31 +0530

    Update __main__.py

--- youtubedl/__main__.py
+++ youtubedl/__main__.py
@@ -4,10 +4,9 @@ from youtubedl import ytdl
 
 async def main():
     try:
-        chat_id = "5857041668"
         await ytdl.start()
         print("[Bot] - Ytdl Bot Started")
-        await ytdl.send_message(chat_id, "Bot Started my dear owner bhai!")
+        await ytdl.send_message("5857041668", "Bot Started my dear owner bhai!")
         print("[Owner] - Started message sented to owner.")
     except Exception as e:
         print(f"{e}")
