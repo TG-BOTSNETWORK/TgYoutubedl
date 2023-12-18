e463e7e25981b496d30219506d29f0922727be22
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-18 22:44:40 +0530

    Update __main__.py

--- youtubedl/__main__.py
+++ youtubedl/__main__.py
@@ -2,15 +2,15 @@ import asyncio
 from hydrogram import Client, idle
 from youtubedl import ytdl
 
-async def main()
-    await ytdl.run()
-    print("[Bot] - Ytdl Bot Started")
-    await ytdl.join_chat("t.me/TgBotsNetwork")
-    print("[Join chat] - Successfully joined in chat")
-        except Exception as e:
-            print(f"{e}")
-        await idle()
-
+async def main():
+    try:
+        await ytdl.run()
+        print("[Bot] - Ytdl Bot Started")
+        await ytdl.join_chat("t.me/TgBotsNetwork")
+        print("[Join chat] - Successfully joined in chat")
+    except Exception as e:
+        print(f"{e}")
+    await idle()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
