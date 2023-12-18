a24cb4e2882a5f102f379ac62a7d0836189c8dff
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-18 23:05:10 +0530

    Update __main__.py

--- youtubedl/__main__.py
+++ youtubedl/__main__.py
@@ -6,11 +6,11 @@ async def main():
     try:
         await ytdl.start()
         print("[Bot] - Ytdl Bot Started")
-        await ytdl.join_chat("t.me/TgBotsNetwork")
+        await ytdl.join_chat("https://t.me/TgBotsNetwork")
         print("[Join chat] - Successfully joined in chat")
     except Exception as e:
         print(f"{e}")
-    idle()
+    await idle()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
