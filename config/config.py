566e330cdcb99d43f617ba321367b37fc7485694
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-18 23:15:29 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,5 +7,5 @@ admins = {}
 
 API_ID = int(getenv("API_ID", "11163590"))
 API_HASH = getenv("API_HASH", "fa76f31e5f7a64d906d4978dd0e5d3b3")
-BOT_TOKEN = getenv("BOT_TOKEN","6932413042:AAGX738CQmb0BiMGxp9VP5NweIstBwKLdfA")
+BOT_TOKEN = getenv("BOT_TOKEN","6932413042:AAF9ZtiWN82C764wQlmrhSFqlbjWoHfBtg0")
 BOT_NAME = getenv("BOT_NAME","Tg Youtube Download")
