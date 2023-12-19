7d8718f0a0193bfb69ed74149ffc948ce9703df9
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-19 17:42:05 +0000

    Update nrml_download.py

--- youtubedl/modules/nrml_download.py
+++ youtubedl/modules/nrml_download.py
@@ -37,6 +37,15 @@ def download_video(video_id, quality="best"):
     }
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+        
+def download_audio(video_id):
+    url = f"https://www.youtube.com/watch?v={video_id}"
+    ydl_opts = {
+        "format": "bestaudio",
+        "outtmpl": f"{DOWNLOAD_DIR}/{video_id}.mp3",
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
 @ytdl.on_message(filters.incoming & filters.text)
 def handle_text_message(client, message):
@@ -70,20 +79,18 @@ def download_video_callback(client, callback_query):
         ytdl.send_video(chat_id, video=file_path, caption="Here is your video.")
         os.remove(file_path)
 
-@ytdl.on_callback_query(filters.regex(r"download_audio:(\S+):(\S+)"))
+@ytdl.on_callback_query(filters.regex(r"download_audio:(\S+)"))
 def download_audio_callback(client, callback_query):
-    _, video_id, download_type = callback_query.data.split(":")
+    video_id = callback_query.matches[0].group(1)
     chat_id = callback_query.message.chat.id
+    ytdl.send_message(chat_id, text="Wait! Your audio is being found...")
 
-    if download_type == "audio":
-        ytdl.send_message(chat_id, text="Wait! Your audio is being found...")
+    download_audio(video_id)
 
-        download_video(video_id, "bestaudio")
-
-        file_path = f"{DOWNLOAD_DIR}/{video_id}.mp3"
-        if os.path.exists(file_path):
-            with open(file_path, "rb") as audio_file:
-                ytdl.send_audio(chat_id, audio=audio_file, caption="Here is your audio.")
-            os.remove(file_path)
-        else:
-            ytdl.send_message(chat_id, text="Error: Audio file not found.")
+    file_path = f"{DOWNLOAD_DIR}/{video_id}.mp3"
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as audio_file:
+            ytdl.send_audio(chat_id, audio=audio_file, caption="Here is your audio.")
+        os.remove(file_path)
+    else:
+        ytdl.send_message(chat_id, text="Error: Audio file not found.")
