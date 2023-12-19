a24713f1a02ec6fc75808218122478c295ec0953
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-19 12:46:32 +0530

    Update start.py

--- youtubedl/modules/start.py
+++ youtubedl/modules/start.py
@@ -9,9 +9,9 @@ from youtubedl import ytdl
 start_keyboard = KeyboardMarkup([
     KeyboardButton("📥 Normal Download", callback_data="nrml_dl"),
     KeyboardButton("📂 Playlist Download", callback_data="plylist_dl"),
-], [
+    ],[
     KeyboardButton("⚙️ Settings", callback_data="settings")
-])
+    ])
 
 
 @ytdl.on_message(filters.command("start"))
@@ -20,5 +20,5 @@ async def start_handler(client: Client, msg: Msg):
         text=f"**👋Hello {msg.from_user.mention()}**\nWelcome, I am a YouTube downloader bot. I can download YouTube videos or audios by searching and providing links and playlist links.👀\n\n**Developed By**: @TgBotsNetwork",
         reply_markup=start_keyboard
     )
-else:
-    await msg.reply_text("Start me in Dm")
+    else:
+        await msg.reply_text("Start me in Dm")
