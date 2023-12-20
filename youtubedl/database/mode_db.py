c67a5994e3b246b89c21c3997732c7ca4c6856ef
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-20 22:01:44 +0530

    Create mode_db.py

---
+++ youtubedl/database/mode_db.py
@@ -0,0 +1,28 @@
+import pymongo
+from config.config import DB_URI
+
+mongo_client = pymongo.MongoClient(DB_URI)
+db = mongo_client["playlistmode"]
+user_mode = db["user_settings"]
+
+def get_normal_download_status(user_id):
+    user_data = user_mode.find_one({"_id": user_id})
+    return user_data.get("normal_download", "Off") if user_data else "Off"
+
+def set_normal_download_status(user_id, status):
+    user_mode.update_one(
+        {"_id": user_id},
+        {"$set": {"normal_download": status}},
+        upsert=True
+    )
+
+def get_playlist_download_status(user_id):
+    user_data = user_mode.find_one({"_id": user_id})
+    return user_data.get("playlist_download", "Off") if user_data else "Off"
+
+def set_playlist_download_status(user_id, status):
+    user_mode.update_one(
+        {"_id": user_id},
+        {"$set": {"playlist_download": status}},
+        upsert=True
+    )
