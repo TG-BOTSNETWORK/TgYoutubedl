f8d5f78650512fc4e2ec5b7f9d6d4168b6819e5a
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-20 23:43:09 +0530

    Update mode_db.py

--- youtubedl/database/mode_db.py
+++ youtubedl/database/mode_db.py
@@ -1,30 +1,37 @@
-import pymongo
-from config.config import DB_URI
+from yutubedl.database import Connect
 
-mongo_client = pymongo.MongoClient(DB_URI)
-db = mongo_client["playlistmode"]
-user_mode = db["user_settings"]
 
-def get_normal_download_status(user_id):
-    user_data = user_mode.find_one({"_id": user_id})
-    status = user_data.get("normal_download") if user_data else None
-    return status
+create_download_status_table = """
+CREATE TABLE IF NOT EXISTS download_status (
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT UNIQUE NOT NULL,
+    normal_download_status BOOLEAN NOT NULL,
+    playlist_download_status BOOLEAN NOT NULL
+);
+"""
+Connect(create_download_status_table)
 
 def set_normal_download_status(user_id, status):
-    user_mode.update_one(
-        {"_id": user_id},
-        {"$set": {"normal_download": status}},
-        upsert=True
-    )
-
-def get_playlist_download_status(user_id):
-    user_data = user_mode.find_one({"_id": user_id})
-    status = user_data.get("playlist_download") if user_data else None
-    return status
+    query = "INSERT INTO download_status (user_id, normal_download_status, playlist_download_status) VALUES (%s, %s, false) ON CONFLICT (user_id) DO UPDATE SET normal_download_status = %s RETURNING id;"
+    result = Connect(query, (user_id, "on" in status, "on" in status), fetch=True)
+    return result[0][0] if result else None
 
 def set_playlist_download_status(user_id, status):
-    user_mode.update_one(
-        {"_id": user_id},
-        {"$set": {"playlist_download": status}},
-        upsert=True
-    )
+    query = "INSERT INTO download_status (user_id, normal_download_status, playlist_download_status) VALUES (%s, false, %s) ON CONFLICT (user_id) DO UPDATE SET playlist_download_status = %s RETURNING id;"
+    result = Connect(query, (user_id, "on" in status, "on" in status), fetch=True)
+    return result[0][0] if result else None
+
+def get_normal_download_status(user_id):
+    query = "SELECT normal_download_status FROM download_status WHERE user_id = %s;"
+    result = Connect(query, (user_id,), fetch=True)
+    return result[0][0] if result else None
+
+def get_playlist_download_status(user_id):
+    query = "SELECT playlist_download_status FROM download_status WHERE user_id = %s;"
+    result = Connect(query, (user_id,), fetch=True)
+    return result[0][0] if result else None
+
+def get_download_status(user_id):
+    query = "SELECT normal_download_status, playlist_download_status FROM download_status WHERE user_id = %s;"
+    result = Connect(query, (user_id,), fetch=True)
+    return result[0] if result else None
