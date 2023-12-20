2845505387c28fce3a53d82bee119dc674e31b82
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-20 23:37:25 +0530

    Update __init__.py

--- youtubedl/database/__init__.py
+++ youtubedl/database/__init__.py
@@ -19,25 +19,3 @@ def Connect(query, values=None, fetch=False):
     finally:
         cursor.close()
         connection.close()
-
-
-create_download_status_table = """
-CREATE TABLE IF NOT EXISTS download_status (
-    id SERIAL PRIMARY KEY,
-    user_id BIGINT UNIQUE NOT NULL,
-    normal_download_status BOOLEAN NOT NULL,
-    playlist_download_status BOOLEAN NOT NULL
-);
-"""
-Connect(create_download_status_table)
-
-def set_normal_download_status(user_id, status):
-    query = "INSERT INTO download_status (user_id, normal_download_status, playlist_download_status) VALUES (%s, %s, false) ON CONFLICT (user_id) DO UPDATE SET normal_download_status = %s RETURNING id;"
-    result = Connect(query, (user_id, "on" in status, "on" in status), fetch=True)
-    return result[0][0] if result else None
-
-def set_playlist_download_status(user_id, status):
-    query = "INSERT INTO download_status (user_id, normal_download_status, playlist_download_status) VALUES (%s, false, %s) ON CONFLICT (user_id) DO UPDATE SET playlist_download_status = %s RETURNING id;"
-    result = Connect(query, (user_id, "on" in status, "on" in status), fetch=True)
-    return result[0][0] if result else None
-
