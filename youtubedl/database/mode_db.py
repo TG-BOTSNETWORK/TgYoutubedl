069a80c0b126cba604347001204e6e06e51ca02c
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-21 12:38:23 +0530

    Update mode_db.py

--- youtubedl/database/mode_db.py
+++ youtubedl/database/mode_db.py
@@ -12,12 +12,9 @@ Connect(create_download_status_table)
 
 def save_on_off(user_id, status, mode):
     column_name = f"{mode}_download_status"
-    status = status if status is not None else False
-    status = status if status is not None else False
-    query = f"INSERT INTO download_status (user_id, {column_name}) VALUES (%s, %s) ON CONFLICT (user_id) DO UPDATE SET {column_name} = %s RETURNING {column_name};"
+    query = f"INSERT INTO download_status (user_id, {column_name}) VALUES (%s, COALESCE(%s, FALSE)) ON CONFLICT (user_id) DO UPDATE SET {column_name} = COALESCE(%s, FALSE) RETURNING {column_name};"
     result = Connect(query, (user_id, status, status), fetch=True)
     return result[0] if result else None
-
 
 
 def get_is_on_off(user_id, mode):
