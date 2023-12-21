0fba5a949d6408850c0cb6ba1ac3d3a2d096a78f
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-21 12:06:45 +0530

    Update mode_db.py

--- youtubedl/database/mode_db.py
+++ youtubedl/database/mode_db.py
@@ -10,22 +10,12 @@ CREATE TABLE IF NOT EXISTS download_status (
 """
 Connect(create_download_status_table)
 
-def save_nrml_on_off(user_id, status):
+def save_on_off(user_id, status):
     query = "INSERT INTO download_status (user_id, normal_download_status, playlist_download_status) VALUES (%s, %s, false) ON CONFLICT (user_id) DO UPDATE SET normal_download_status = %s RETURNING normal_download_status, playlist_download_status;"
     result = Connect(query, (user_id, status, status), fetch=True)
     return result[0] if result else None
 
-def save_playlist_on_off(user_id, status):
-    query = "INSERT INTO download_status (user_id, normal_download_status, playlist_download_status) VALUES (%s, false, %s) ON CONFLICT (user_id) DO UPDATE SET playlist_download_status = %s RETURNING normal_download_status, playlist_download_status;"
-    result = Connect(query, (user_id, status, status), fetch=True)
-    return result[0] if result else None
-
-def get_is_nrml_on_off(user_id):
+def get_is_on_off(user_id):
     query = "SELECT normal_download_status FROM download_status WHERE user_id = %s;"
-    result = Connect(query, (user_id,), fetch=True)
-    return result[0] if result else None
-
-def get_is_playlist_on_off(user_id):
-    query = "SELECT playlist_download_status FROM download_status WHERE user_id = %s;"
     result = Connect(query, (user_id,), fetch=True)
     return result[0] if result else None
