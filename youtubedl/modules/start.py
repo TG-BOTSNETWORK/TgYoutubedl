a6484e96ad9641eb655a00bb6caa79ef37f4af69
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-21 11:56:53 +0530

    Update start.py

--- youtubedl/modules/start.py
+++ youtubedl/modules/start.py
@@ -9,8 +9,8 @@ from youtubedl import ytdl
 from youtubedl.database.mode_db import (
     save_nrml_on_off,
     save_playlist_on_off,
-    get_is_nrml__on_off,
-    get_is_playlist__on_off
+    get_is_nrml_on_off,
+    get_is_playlist_on_off
 )
 
 start_keyboard = KeyboardMarkup([[
