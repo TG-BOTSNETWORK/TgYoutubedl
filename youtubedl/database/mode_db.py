ebedc0fe8388066687711c1df9d086478abfe291
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2023-12-21 12:55:29 +0530

    Update mode_db.py

--- youtubedl/database/mode_db.py
+++ youtubedl/database/mode_db.py
@@ -1,4 +1,5 @@
 from youtubedl.database import Connect
+import inspect
 
 create_download_status_table = """
 CREATE TABLE IF NOT EXISTS download_status (
@@ -10,15 +11,15 @@ CREATE TABLE IF NOT EXISTS download_status (
 """
 Connect(create_download_status_table)
 
-def save_on_off(user_id, status, mode):
-    column_name = f"{mode}_download_status"
+def save_on_off(user_id, status):
+    column_name = f"{inspect.currentframe().f_code.co_name.split('_')[1]}_download_status"
     status = status if status is not None else False
     query = f"INSERT INTO download_status (user_id, {column_name}) VALUES (%s, %s) ON CONFLICT (user_id) DO UPDATE SET {column_name} = %s RETURNING {column_name};"
     result = Connect(query, (user_id, status, status), fetch=True)
     return result[0] if result else None
 
-def get_is_on_off(user_id, mode):
-    column_name = f"{mode}_download_status"
+def get_is_on_off(user_id):
+    column_name = f"{inspect.currentframe().f_code.co_name.split('_')[1]}_download_status"
     query = f"SELECT {column_name} FROM download_status WHERE user_id = %s;"
     result = Connect(query, (user_id,), fetch=True)
     return result[0] if result else None
