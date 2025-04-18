df2031082fbb58ed528bbf2aa1fab930b0f16805
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2025-04-18 12:47:12 +0530

    Update nrml_download.py

--- youtubedl/modules/nrml_download.py
+++ youtubedl/modules/nrml_download.py
@@ -1,13 +1,23 @@
 import os
+import os
+from hydrogram import Client, filters
+from hydrogram.types import (
+    InlineKeyboardButton as Button,
+    InlineKeyboardMarkup as Markup,
+)
+from youtube_search import YoutubeSearch
+from yt_dlp import YoutubeDL
+from youtubedl import ytdl
+import yt_dlp
 import re
 import asyncio
 import time
 import requests
 from PIL import Image
-from hydrogram import Client, filters
-from hydrogram.types import InlineKeyboardButton as Button, InlineKeyboardMarkup as Markup
-from youtubedl.database.mode_db import save_on_off, get_is_on_off
-import yt_dlp
+from youtubedl.database.mode_db import (
+    save_on_off,
+    get_is_on_off
+)
 
 def extract_video_id(url):
     match = re.search(r"(?:v=|\/videos\/|embed\/|youtu.be\/|\/v\/|\/e\/|watch\?v=|&v=|%2Fvideos%2F|%2Fwatch%3Fv%3D|%2F|\?v=)([^#\\&\?]*)(?:[\w-]+)?", url)
