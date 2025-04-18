910869267e4ecb1e10293a719d704d9529b90936
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2025-04-18 12:17:49 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,6 +7,6 @@ admins = {}
 
 API_ID = int(getenv("API_ID", "11163590"))
 API_HASH = getenv("API_HASH", "fa76f31e5f7a64d906d4978dd0e5d3b3")
-BOT_TOKEN = getenv("BOT_TOKEN","6932413042:AAGPXVvxjr6Tzhq4NNVCNX8_eblq2wq9Hz4")
+BOT_TOKEN = getenv("BOT_TOKEN","6932413042:AAGw1x_bDTY78T9s57tVEz6Yy6Iv_21y_Qk")
 BOT_NAME = getenv("BOT_NAME","Tg Youtube Download")
-DB_URI = getenv("DB_URI", "postgres://doqtmbqn:6vuttQBYuDdh-Gts7A8J21CEBh7lvByp@berry.db.elephantsql.com/doqtmbqn")
+DB_URI = getenv("DB_URI", "mongodb+srv://VEEZWORKS:SANTHU7981@veezworks.stpqs.mongodb.net/?retryWrites=true&w=majority&appName=veezworks")
